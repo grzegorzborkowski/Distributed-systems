65a5219131cd191ea9991fffe51279458ed8e21a
gborkowski <gb.grzegorzborkowski@gmail.com>
2017-03-24 11:42:53 +0100

    Add jgroups-chat initial commit

---
+++ jgroups-chat/src/Main.java
@@ -0,0 +1,7 @@
+import pl.edu.agh.dsrg.sr.chat.protos.ChatOperationProtos;
+
+public class Main {
+    public static void main(String[] args) {
+        ChatOperationProtos chatOperationProtos;
+    }
+}
